3c037fe0b3a6760fdeea787803871c5e001b5440
maxxxara <dati.maxarashvili24@gmail.com>
2023-07-25 23:38:01 +0400

    vercel7

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 400;
+import path from "path";
+
 // const cors = require("cors");
 
 const nodeHtmlToImage = require("node-html-to-image");
